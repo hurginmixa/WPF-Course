182c89abeaf8979cca1a320d234687a57c3c91a9
Michael Hurgin <mixah@camtek.com>
2024-09-04 15:36:09 +0300

    Canvas Demo

---
+++ WpfCourseProjects/CanvasDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanvasDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += (s, e) =>
+            {
+                Canvas canvas = new Canvas();   
+                canvas.Width = 300;
+                canvas.Height = 300;
+                canvas.Background = new SolidColorBrush(color: Colors.Yellow);
+
+                Ellipse ellipse = new Ellipse();
+                ellipse.Fill = new SolidColorBrush(Colors.Red);
+                ellipse.Width = 50;
+                ellipse.Height = 150;
+
+                canvas.Children.Add(ellipse);
+
+                Canvas.SetTop(ellipse, 20);
+                Canvas.SetLeft(ellipse, 20);
+
+                this.MainGrid.Children.Add(canvas);
+            };
+        }
+    }
+}
