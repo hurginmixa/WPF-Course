d86a43df3933164531c5c5db267c07e8456597ff
Michael Hurgin <mixah@camtek.com>
2024-09-04 11:30:18 +0300

    First step

---
+++ WpfCourseProjects/WpfApp1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+    }
+}
