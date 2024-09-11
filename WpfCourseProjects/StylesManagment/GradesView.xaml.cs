5182419ea31630667d3495cdc1bff96969c41c0c
Michael Hurgin <mixah@camtek.com>
2024-09-11 13:46:48 +0300

    Style and resource
    (cherry picked from commit f8258342c3066d59400e24af70792d33efff1ee3)

---
+++ WpfCourseProjects/StylesManagment/GradesView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StylesManagment
+{
+    /// <summary>
+    /// Interaction logic for GradesView.xaml
+    /// </summary>
+    public partial class GradesView : Window
+    {
+        public GradesView()
+        {
+            InitializeComponent();
+
+            int[] grdes = new int[] { 10, 100, 85, 78, 95};
+
+            Box.ItemsSource = grdes;
+        }
+    }
+}
---
+++ WpfCourseProjects/StylesManagment/Selectors/GradeStyleSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StylesManagment.Selectors
+{
+    internal class GradeStyleSelector : StyleSelector
+    {
+        public Style GoodStyle { get; set; }
+
+        public Style BadStyle { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            int countItem = (int)item;
+
+            return countItem > 80 ? GoodStyle : BadStyle;
+        }
+    }
+}
