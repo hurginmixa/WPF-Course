442b14cadd9730ea9f55a3a467f9b9f92917178c
Michael Hurgin <mixah@camtek.com>
2024-09-11 16:14:55 +0300

    DependProperty and Events

---
+++ WpfCourseProjects/Events/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            grid.MouseLeftButtonDown += OnMouseLeftButtonDown;
+            border.MouseLeftButtonDown += OnMouseLeftButtonDown;
+            sp.MouseLeftButtonDown += OnMouseLeftButtonDown;
+            el.MouseLeftButtonDown += OnMouseLeftButtonDown;
+
+            grid.MouseEnter += OnMouseEnter;
+            border.MouseEnter += OnMouseEnter;
+            sp.MouseEnter += OnMouseEnter;
+            el.MouseEnter += OnMouseEnter;
+        }
+
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine($"MouseEnter {sender.ToString()}");
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine($"MouseLeftButtonDown {sender.ToString()}");
+        }
+    }
+}
---
+++ WpfCourseProjects/Events/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Events
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            ug.AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(Handler));
+        }
+
+        private void Handler(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(message: $"Click >> {e.OriginalSource.ToString()}");
+        }
+    }
+}
